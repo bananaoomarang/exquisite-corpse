a2780a52ebee3865dda4c6c98c7823bb0f1e8bf0
Milo Mordaunt <milomord@gmail.com>
2017-11-30 11:40:36 -0500

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-# Probably a nicer way of doing this
-# @ me
-git co gh-pages
-git reset --hard master
 ENV=production lein do clean, cljsbuild once min
-rm -rf dev src target figwheel_server.log LICENSE project.clj README.md deploy.sh
-cp -rv resources/public/* .
-rm -rf resources
-git add .
-git ci -m "new deploy $(date)"
-git push -f origin gh-pages
-git co master
+rsync -avz ./resources/public milo@wordsports.xyz:/var/www/exquisite-corpse
