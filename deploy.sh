ab9c61070b89cad610fab30dd26c8d8da4031315
Milo Mordaunt <milomord@gmail.com>
2017-11-29 22:44:30 -0500

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Probably a nicer way of doing this
+# @ me
+git co gh-pages
+git reset --hard master
+lein do clean, cljsbuild once min
+rm -rf dev src target figwheel_server.log LICENSE project.clj README.md deploy.sh
+cp -rv resources/public/* .
+rm -rf resources
+git add .
+git ci -m "new deploy $(date)"
+git push -f origin gh-pages
+git co master
